b0be2e4512570e3ad2471ff658efc7d7af1111dc
Panchatcharam <aravinthkumar.panchatcharam@sap.com>
2020-10-11 17:35:13 +0530

    added marker

--- demo/src/main/java/com/cflog/demo/DemoApplication.java
+++ demo/src/main/java/com/cflog/demo/DemoApplication.java
@@ -3,16 +3,20 @@ package com.cflog.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
 import org.slf4j.Logger;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class DemoApplication {
 
+	private static String APP_START_UP = "Application Start-Up";
 	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+	private static final Marker MARKER = MarkerFactory.getMarker(APP_START_UP);
 
 	public static void main(String[] args) {
-		LOGGER.info("In the main method");
+		LOGGER.info(MARKER, "In the main method");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- demo/src/main/java/com/cflog/demo/GreetingController.java
+++ demo/src/main/java/com/cflog/demo/GreetingController.java
@@ -8,20 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 @RestController
 public class GreetingController {
-
+    private static String GREETING_READ = "Greetings Read";
     private static final Logger LOGGER = LoggerFactory.getLogger(GreetingController.class);
+    private static final Marker MARKER = MarkerFactory.getMarker(GREETING_READ);
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        LOGGER.info("Fetching the greeting for you");
-        LOGGER.debug("Counter of the greeting {} ", counter.incrementAndGet());
-        LOGGER.debug("Debug log triggered");
+        LOGGER.info(MARKER, "Fetching the greeting for you");
+        LOGGER.debug(MARKER, "Counter of the greeting {} ", counter.incrementAndGet());
+        LOGGER.debug(MARKER, "Debug log triggered");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
 
     }
